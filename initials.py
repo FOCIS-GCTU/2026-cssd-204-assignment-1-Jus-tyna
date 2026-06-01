cd6d92b1bb4ba7586dd5c37108eb7dfe27e31cd3
Jus-tyna <2425401354@live.gctu.edu.gh>
2026-06-01 16:23:47 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+#File: Initials.py
+#Description: Print out my initials.
+#Assignment Number: 1
+#
+#Name: Justina Padikwor Blanney
+#STUDENT ID: 2425401354
+#Email: 2425401354@live.gctu.edu.gh
+#Grader:Augustus
+#
+# On my honor, Justina Padikwor Blanney, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #Code to print out my initials JPB
 
+    print ("print out my initials\n\n")
+
+    print ("...JPB\n\n")
+
+    print()
+
+    print("...JJJJJJJJJJJJ.......PPPPPPPPPPPP........BBBBBBBBBBBB......")
+    print("...JJJJJJJJJJJJ.......PPPPPPPPPPPP........BBBBBBBBBBBBB.....")
+    print(".......JJJJ...........PP........PP........BB.........BB.....")
+    print(".......JJJJ...........PP........PP........BB.........BB.....")
+    print(".......JJJJ...........PPPPPPPPPPPP........BBBBBBBBBBBBB.....")
+    print(".......JJJJ...........PPPPPPPPPPPP........BBBBBBBBBBBB......")
+    print("...J...JJJJ...........PP..................BB.........BB.....")
+    print("...JJ..JJJJ...........PP..................BB.........BB.....")
+    print("...JJJJJJJJ......**...PP.............**...BBBBBBBBBBBBB...**")
+    print("....JJJJJJ.......**...PP.............**...BBBBBBBBBBBB....**")
+
+    print()
 
 
 main()
